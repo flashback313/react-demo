03cd3e5f157d025d5bd1301dcb6b86414a003d35
flashback313 <windmark2012@gmail.com>
2016-05-19 10:39:36 +0800

    add redux

--- entry.jsx
+++ entry.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
 
 
 // import LikeButton from './components/LikeButton/index';
@@ -7,10 +10,18 @@ import {render} from 'react-dom';
 // import InputName from './components/InputName/index';
 // import AddButton from './components/AddButton/index';
 // import AddBar from './components/AddBar/index';
-import TitleChange from './components/TitleChange/index';
+// import TitleChange from './components/TitleChange/index';
+import App from './components/Todos/App'
 
-var titleContext = "My Title Component";
+
+// render(
+//     <TitleChange title="My Title Component" />,
+//     document.getElementById('appwrap')
+// );
+let store = createStore(todoApp)
 render(
-    <TitleChange title="My Title Component" />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
     document.getElementById('appwrap')
 );
